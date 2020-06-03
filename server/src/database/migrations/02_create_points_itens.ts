dcdd91088bdf3a58180c7fc2be42c02a084841dc
karlo <karlomr@gmail.com>
2020-06-02 23:59:42 -0300

    2 dia

---
+++ server/src/database/migrations/02_create_points_itens.ts
@@ -0,0 +1,25 @@
+import Knex from 'knex';
+
+//cria tabela
+export async function up(knex: Knex){
+    return knex.schema.createTable('point_items', table => {
+        table.increments('id').primary();
+        
+        table.integer('point_id')
+            .notNullable()
+            .references('id')
+            .inTable('points');
+
+        table.integer('item_id')
+            .notNullable()
+            .references('id')
+            .inTable('items');
+
+    });
+}
+
+//volta atras e deleta tabela
+export async function down(knex: Knex){
+    return knex.schema.dropTable('points_items');
+}
+
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,56 +1,15 @@
-import express, { response } from 'express';
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import routes from  './routes';
 
 const app = express();
 
+
+app.use(cors());
 app.use(express.json());
+app.use(routes);
 
-//POST http://localhost:3333/users = Cria user
-//GET http://localhost:3333/users = Lista user
-//GET http://localhost:3333/users/5 = Busca dados user ID 5
-
-//Request Param: embutido na rota, identifica o recurso
-//Query Param: embutido na rota, opcao para filtros, paginação
-//Request body: parametros para criação/atualização de info
-
-//SELECT * FOM users WHERE name = 'Karlo'
-//knex('users').where('name', 'Karlo').select('*')
-
-
-const users = [
-    'Karlo',
-    'Mesquita',
-    'Rodrigues',
-    'karlo',
-];
-
-app.get('/users', (request, response) =>{
-    //console.log('Listagem de usuários');
-    //response.send({'Hello World!'})
-    const search = String(request.query.search);
-
-    const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
-
-    response.json(filteredUsers);
-});
-
-app.get('/users/:id', (request, response) => {
-    const id = Number(request.params.id);
-
-    const user = users[id];
-
-    return response.json(user);
-});
-
-app.post('/users',(request, response) => {
-    const data = request.body;
-
-
-    const user = {
-        name: data.name,
-        email: data.email,
-    }
-
-    response.json(user);
-});
+app.use('/uploads', express.static(path.resolve(__dirname,'..','uploads')))
 
 app.listen(3333);
